0dde6a83939ed9c3f892724dcc5d948337958996
Yuna Kim <111223326+Yunn-k@users.noreply.github.com>
2024-11-14 15:40:29 +0900

    [level 0] Title: 홀짝 구분하기, Time: 13.27 ms, Memory: 7.59 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -1,6 +1,4 @@
 a = int(input())
 
-if (a%2 == 0):
-    print(f'{a} is even')
-else:
-    print(f'{a} is odd')
+print(f"{a} is {'even' if a%2 == 0 else 'odd'}")
+#print문 내에 삼항연산자 처리 가능. 단 변경값은 {변수}로 묶어줌
