2e306502e689fe78edaada0c33a4362bbf7d1bf7
Yuna Kim <111223326+Yunn-k@users.noreply.github.com>
2024-11-24 00:09:43 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -4,9 +4,12 @@ def solution(a, b, c):
     # 중복값을 지우는 set의 성질을 활용하여 체크
     check = len(set([a,b,c]))
     
-    if check == 1:
-        answer = (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
-    else:
-        answer = (a + b + c) * (a**2 + b**2 + c**2) if check == 2 else (a + b + c)
+    #기본값인 a+b+c를 변수로 처리
+    answer = a + b + c
     
-    return answer
+    if check == 1:
+        return answer * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    elif check == 2:
+        return answer * (a**2 + b**2 + c**2)
+    else:
+        return answer
