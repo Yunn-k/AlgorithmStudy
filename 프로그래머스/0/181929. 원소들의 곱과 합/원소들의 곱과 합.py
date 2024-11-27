976af8b3d82ec2f91d13ebfcfe951aad4e04b58f
Yuna Kim <111223326+Yunn-k@users.noreply.github.com>
2024-11-27 13:46:21 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,8 +1,5 @@
-def solution(num_list):
-    mul = 1 #곱셈은 0으로 초기화하면 안됨
-    sum_ = 0
-    for n in num_list:
-        mul *= n
-        sum_ += n
+from math import prod
+# math에 내장된 prod로 리스트의 곱셈값을 반환
 
-    return 1 if mul < sum_**2 else 0
+def solution(num_list):
+    return 1 if prod(num_list) < sum(num_list)**2 else 0
