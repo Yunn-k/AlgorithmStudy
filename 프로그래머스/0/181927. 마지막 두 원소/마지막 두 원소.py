c950628c673e57ba932a1bfedf455f28dc57a9db
Yuna Kim <111223326+Yunn-k@users.noreply.github.com>
2024-12-03 10:58:37 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,13 +1,7 @@
 def solution(num_list):
     answer = []
     
-    the_last = num_list[len(num_list)-1];
-    before_last = num_list[len(num_list)-2];
+    last = num_list[-1];
+    before_last = num_list[-2];
     
-    if(the_last > before_last):
-        # return num_list.append(the_last - before_last) #append메서드는 요소를 추가하고 반환이 none이므로 이렇게 한번에 리턴하면 안됨
-        num_list.append(the_last - before_last)
-    else:
-        num_list.append(the_last * 2)
-
-    return num_list;
+    return num_list + [last - before_last] if last > before_last else num_list + [last * 2];
